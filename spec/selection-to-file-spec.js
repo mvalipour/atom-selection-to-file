9a84c877c1913d479fd3b076f686f7b188a3b101
Mo Valipour <mo.valipour@deliveroo.co.uk>
2017-07-22 15:22:50 +0200

    Add specs + minor improvements

--- spec/selection-to-file-spec.js
+++ spec/selection-to-file-spec.js
@@ -1,6 +1,6 @@
 'use babel';
 
-import SelectionToFile from '../lib/selection-to-file';
+import Module from '../lib/selection-to-file';
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
@@ -8,65 +8,196 @@ import SelectionToFile from '../lib/selection-to-file';
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
 describe('SelectionToFile', () => {
-  let workspaceElement, activationPromise;
+  let editor, editorView, activationPromise;
 
-  beforeEach(() => {
-    workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('selection-to-file');
-  });
+  function fileIsOpen(filename) {
+    waitsForPromise(() => atom.workspace.open(filename));
+    runs(() => {
+      editor = atom.workspace.getActiveTextEditor();
+      editorView = atom.views.getView(editor);
+      activationPromise = atom.packages.activatePackage('selection-to-file');
+    });
+  }
 
-  describe('when the selection-to-file:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.selection-to-file')).not.toExist();
+  function waitForCommandCompletion() {
+    const mainModule = atom.packages.getActivePackage('selection-to-file').mainModule;
+    return new Promise((resolve) => {
+      const timer = setInterval(() => {
+        if (mainModule.state === 'completed') {
+          resolve();
+          clearInterval(timer);
+        }
+      }, 10);
+    });
+  }
 
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'selection-to-file:toggle');
+  function commandIsSentAndCompleted(command) {
+    atom.commands.dispatch(editorView, `selection-to-file:${command}`);
+    waitsForPromise(() => activationPromise);
+    waitsForPromise(waitForCommandCompletion)
+  }
 
-      waitsForPromise(() => {
-        return activationPromise;
+  describe(':create command', () => {
+    describe('when a ruby file is open', () => {
+      beforeEach(() => fileIsOpen('test_file.rb'));
+
+      describe('when no text is selected and cursor is not on a word either', () => {
+        it('does not open new editor', () => {
+          editor.insertText("class Test\r  SomeOtherClass\rend");
+          editor.setCursorBufferPosition([1, 1]);
+          commandIsSentAndCompleted('create');
+          runs(() => {
+            expect(atom.workspace.getTextEditors().length).toEqual(1);
+          });
+        });
       });
 
-      runs(() => {
-        expect(workspaceElement.querySelector('.selection-to-file')).toExist();
+      describe('when no text is selected and cursor is on a word', () => {
+        it('does not open new editor', () => {
+          editor.insertText("class Test\r  SomeOtherClass\rend");
+          editor.setCursorBufferPosition([1, 4]);
+          commandIsSentAndCompleted('create');
+          runs(() => {
+            expect(atom.workspace.getTextEditors().length).toEqual(2);
 
-        let selectionToFileElement = workspaceElement.querySelector('.selection-to-file');
-        expect(selectionToFileElement).toExist();
+            const newEditor = atom.workspace.getTextEditors()[1];
+            expect(newEditor.getPath().endsWith('/some_other_class.rb'))
+              .toBe(true);
 
-        let selectionToFilePanel = atom.workspace.panelForItem(selectionToFileElement);
-        expect(selectionToFilePanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'selection-to-file:toggle');
-        expect(selectionToFilePanel.isVisible()).toBe(false);
+            expect(newEditor.getText()).toEqual("# SomeOtherClass");
+          });
+        });
+      });
+
+      describe('when there is a text selected', () => {
+        it('creates a new editor with file name from selection and selected text as commented content', () => {
+          editor.insertText("class Test\r  SomeOtherClass\rend");
+          editor.setSelectedBufferRange([[1, 2], [1, 16]]);
+          commandIsSentAndCompleted('create');
+          runs(() => {
+            expect(atom.workspace.getTextEditors().length).toEqual(2);
+
+            const newEditor = atom.workspace.getTextEditors()[1];
+            expect(newEditor.getPath().endsWith('/some_other_class.rb'))
+              .toBe(true);
+
+            expect(newEditor.getText()).toEqual("# SomeOtherClass");
+          });
+        });
+      });
+
+      describe('when there is a text selected with module', () => {
+        it('creates a new editor with dir and file name from selection and selected text as commented content', () => {
+          editor.insertText("class Test\r  Some::Other::Class\rend");
+          editor.setSelectedBufferRange([[1, 2], [1, 20]]);
+          commandIsSentAndCompleted('create');
+          runs(() => {
+            expect(atom.workspace.getTextEditors().length).toEqual(2);
+
+            const newEditor = atom.workspace.getTextEditors()[1];
+            expect(newEditor.getPath().endsWith('/some/other/class.rb'))
+              .toBe(true);
+
+            expect(newEditor.getText()).toEqual("# Some::Other::Class");
+          });
+        });
       });
     });
 
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
+    describe('when a js file is open', () => {
+      beforeEach(() => fileIsOpen('testFile.js'));
 
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
+      describe('when there is a text selected', () => {
+        it('creates a new editor with file name from selection and selected text as commented content', () => {
+          editor.insertText("function\r  SomeOtherClass()\r{}");
+          editor.setSelectedBufferRange([[1, 2], [1, 16]]);
+          commandIsSentAndCompleted('create');
+          runs(() => {
+            expect(atom.workspace.getTextEditors().length).toEqual(2);
 
-      expect(workspaceElement.querySelector('.selection-to-file')).not.toExist();
+            const newEditor = atom.workspace.getTextEditors()[1];
+            expect(newEditor.getPath().endsWith('/some-other-class.js'))
+              .toBe(true);
 
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'selection-to-file:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
+            expect(newEditor.getText()).toEqual("// SomeOtherClass");
+          });
+        });
       });
 
-      runs(() => {
-        // Now we can test for view visibility
-        let selectionToFileElement = workspaceElement.querySelector('.selection-to-file');
-        expect(selectionToFileElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'selection-to-file:toggle');
-        expect(selectionToFileElement).not.toBeVisible();
+      describe('when there is a text selected with namespace', () => {
+        it('creates a new editor with dir and file name from selection and selected text as commented content', () => {
+          editor.insertText("const x = new some.other.Class()");
+          editor.setSelectedBufferRange([[0, 14], [0, 30]]);
+          commandIsSentAndCompleted('create');
+          runs(() => {
+            expect(atom.workspace.getTextEditors().length).toEqual(2);
+
+            const newEditor = atom.workspace.getTextEditors()[1];
+            expect(newEditor.getPath().endsWith('/some/other/class.js'))
+              .toBe(true);
+
+            expect(newEditor.getText()).toEqual("// some.other.Class");
+          });
+        });
+      });
+    });
+
+    describe('when a cs file is open', () => {
+      beforeEach(() => fileIsOpen('TestFile.cs'));
+
+      describe('when there is a text selected', () => {
+        it('creates a new editor with file name from selection and selected text as commented content', () => {
+          editor.insertText("// TODO: some other class to add");
+          editor.setSelectedBufferRange([[0, 9], [0, 25]]);
+          commandIsSentAndCompleted('create');
+          runs(() => {
+            expect(atom.workspace.getTextEditors().length).toEqual(2);
+
+            const newEditor = atom.workspace.getTextEditors()[1];
+            expect(newEditor.getPath().endsWith('/SomeOtherClass.cs'))
+              .toBe(true);
+
+            expect(newEditor.getText()).toEqual("// some other class");
+          });
+        });
+      });
+
+      describe('when there is a text selected with namespace', () => {
+        it('creates a new editor with dir and file name from selection and selected text as commented content', () => {
+          editor.insertText("const x = new Some.Other.Class()");
+          editor.setSelectedBufferRange([[0, 14], [0, 30]]);
+          commandIsSentAndCompleted('create');
+          runs(() => {
+            expect(atom.workspace.getTextEditors().length).toEqual(2);
+
+            const newEditor = atom.workspace.getTextEditors()[1];
+            expect(newEditor.getPath().endsWith('/Some/Other/Class.cs'))
+              .toBe(true);
+
+            expect(newEditor.getText()).toEqual("// Some.Other.Class");
+          });
+        });
+      });
+    });
+
+    describe('when a file with no extension is open', () => {
+      beforeEach(() => fileIsOpen('some-file'));
+
+      describe('when there is a text selected', () => {
+        it('creates a new editor with file name from selection and selected text as commented content', () => {
+          editor.insertText("This is some file with no extension");
+          editor.setSelectedBufferRange([[0, 8], [0, 17]]);
+          commandIsSentAndCompleted('create');
+          runs(() => {
+            expect(atom.workspace.getTextEditors().length).toEqual(2);
+
+            const newEditor = atom.workspace.getTextEditors()[1];
+            expect(newEditor.getPath().endsWith('/some-file'))
+              .toBe(true);
+
+            expect(newEditor.getText()).toEqual("some file");
+          });
+        });
       });
     });
   });
--- spec/selection-to-file-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import SelectionToFileView from '../lib/selection-to-file-view';
-
-describe('SelectionToFileView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
